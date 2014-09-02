3d321dcc360bf4098866660ef6a2a556898463ad
Federico Aloi <federico.aloi@gmail.com>
2014-09-02 18:01:32 -0300

    Saco los tokens de Dropbox

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,10 +6,5 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://localhost/parsimotionsyncer-dev'
-  },
-
-  dropbox: {
-    clientID: 'aptkfvli7ztnm94',
-    clientSecret: 'g8uwx67dmgxg3vv'
   }
 };
