310f17996b40126ca8a11f5d3be1d4bd80b1bb7e
Federico Aloi <federico.aloi@gmail.com>
2014-12-22 17:15:03 -0300

    Agrego un punto y coma que faltaba

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("coffee-script/register");
 
-var app = require("./app.coffee")
+var app = require("./app.coffee");
 
 app.server.listen(app.port, app.ip, function() {
   console.log("Express server listening on %d, in %s mode", app.port, app.env);
