4e87242e29dab516c48b64054b696915418145a3
Rodrigo Alfonso <rodri042@gmail.com>
2015-02-13 20:22:52 -0300

    Saco una task del build para ver si hace el deploy

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ require("coffee-script/register");
 var app = require("./app.coffee");
 
 app.server.listen(app.port, app.ip, function() {
-  console.log("Express server listening on %d, in %s mode", app.port, app.env);
+  console.log(JSON.stringify(app));
 });
